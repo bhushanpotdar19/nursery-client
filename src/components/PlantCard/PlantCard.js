1d63a9d2bdbc5559243926a97a5d443feeeb4bd5
bhushanpotdar19 <bhushanpotdar3@gmail.com>
2024-07-28 18:13:49 +0530

    client

---
+++ src/components/PlantCard/PlantCard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./PlantCard.css"
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { Link } from 'react-router-dom'
+
+function PlantCard({_id, name, category, image, price, description, loadPlants}) {
+  
+  const deletePlant = async (plantId) =>{
+    const response = await axios.delete(`${process.env.REACT_APP_API_URL}/plant/${plantId}`)
+
+    toast.success(response.data.message)
+    loadPlants();
+  }
+  
+  return (
+    <div className='plant-card'>
+        <h1 className='plant-title'>PlantName : {name}</h1>
+        <p className='plant-info'>Price : {price}</p>
+        <p className='plant-info'>Category : {category}</p>
+        <p className='plant-info'>Description : {description}</p>
+        <img src={image} className='plant-card-image' alt='plant-img'/>
+        <div>
+          <Link className='plant-card-action-btn' to={`/update/${_id}`}>Edit</Link>
+          <button type='button' className='plant-card-action-btn' 
+            onClick={()=>{
+              deletePlant(_id)
+              }}>
+                Delete
+          </button>
+        </div>
+    </div>
+  )
+}
+
+export default PlantCard
---
+++ src/views/Home/Home.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import toast, { Toaster } from "react-hot-toast"
+import { Link } from "react-router-dom"
+
+import "./Home.css"
+import PlantCard from "../../components/PlantCard/PlantCard"
+import addImage from "./plus.png"
+
+function Home() {
+
+  const [plants, setPlants] = useState([])
+
+  const loadPlants = async () =>{
+    toast.loading("Loading plants....")
+    const response = await axios.get(`http://localhost:5000/plants`)
+    toast.dismiss()
+    toast.success("Plants loaded successfully")
+    setPlants(response.data.data);
+  }
+
+  useEffect(()=>{
+    loadPlants()
+  }, [])
+
+  return (
+    <>
+      <h1 className="heading">RJ's Nursery</h1>
+      <div className="plantCard-div">
+        {
+          plants.map((plant, i)=>{
+            const {_id, name, category, image, price, description} =plant
+            return <PlantCard key={i} _id={_id} name={name} category={category} image={image} price={price} description={description} loadPlants={loadPlants}/>
+          })
+        }
+        <Toaster/>
+        <Link to="/add">
+          <img src={addImage} alt="addImage" className="addImage-btn"/>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Home
